9ccf47f508c36e3b670d0dc57ba816221057ea94
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-08-20 12:19:16 -0600

    Add foodsRouter, remove userRouter

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var foodsRouter = require('./routes/api/v1/foods');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/foods', foodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
