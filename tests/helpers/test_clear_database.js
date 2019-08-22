c967f0190987f72a0deb36f90b9003f1a744e2d9
billy urrutia <billyurrutia@gmail.com>
2019-08-21 18:52:18 -0600

    added Meals to cleanup

--- tests/helpers/test_clear_database.js
+++ tests/helpers/test_clear_database.js
@@ -1,5 +1,7 @@
 var Food = require('../../models').Food;
+var Meal = require('../../models').Meal;
 
 module.exports = async function cleanup() {
   await Food.destroy({ where: {} })
+  await Meal.destroy({ where: {} })
 }
