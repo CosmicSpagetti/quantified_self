c17c85e0fdf8cd3bcd80418c67491974559ca597
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-08-24 11:13:44 -0600

    Quick fix

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -60,7 +60,7 @@ router.post('/:id/foods/:food_id', (request, response) => {
           return meal.addFood(food)
           .then(() => {
             response.setHeader('Content-Type', 'application/json');
-            response.status(201).send(JSON.stringify({ 'message': `Successfully added ${food.name} to ${meal.name}`})); 
+            response.status(201).send(JSON.stringify({ message: `Successfully added ${food.name} to ${meal.name}`})); 
           })
         } else {
           response.setHeader('Content-Type', 'application/json');
