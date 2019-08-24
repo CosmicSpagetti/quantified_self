ebf9c9bac0100f50cf8928d063920441812d2276
billy urrutia <billyurrutia@gmail.com>
2019-08-24 10:51:51 -0600

    adds post route for adding food to meal

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var Meal = require('../../../models').Meal;
+var Food = require('../../../models').Food;
 var MealFood = require('../../../models').MealFood;
 
 var router = express.Router();
@@ -41,6 +42,42 @@ router.get('/:id/foods', (request, response) => {
   })
 })
 
+router.post('/:id/foods/:food_id', (request, response) => {
+  return Meal.findOne({
+    where: {
+      id: request.params.id
+    }
+  })
+  .then(meal => {
+    if (meal) {
+      return Food.findOne({
+        where: {
+          id: request.params.food_id
+        }
+      })
+      .then(food => {
+        if (food) {
+          return meal.addFood(food)
+          .then(() => {
+            response.setHeader('Content-Type', 'application/json');
+            response.status(201).send(JSON.stringify({ 'message': `Successfully added ${food.name} to ${meal.name}`})); 
+          })
+        } else {
+          response.setHeader('Content-Type', 'application/json');
+          response.status(404).send({ error: 'Food not found.' }); 
+        } 
+      })
+    } else {
+      response.setHeader('Content-Type', 'application/json');
+      response.status(404).send({ error: 'Meal not found.' }); 
+    }
+  })
+  .catch(error => {
+    response.setHeader('Content-Type', 'application/json');
+    response.status(500).send({ error });
+  })
+})
+
 router.delete('/:id/foods/:food_id', (request, response) => {
   return MealFood.findOne({
     where: {
@@ -61,7 +98,6 @@ router.delete('/:id/foods/:food_id', (request, response) => {
     }
   })
   .catch(error => {
-    console.log(error)
     response.setHeader('Content-Type', 'application/json');
     response.status(500).send({ error });
   })
