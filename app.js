cf7e5b009e8e38ba052e77d01ab9a7a51d145182
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-08-28 12:28:34 -0600

    Add food search route, include in app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var foodsRouter = require('./routes/api/v1/foods');
 var mealsRouter = require('./routes/api/v1/meals');
+var foodsSearchRouter = require('./routes/api/v1/foods_search');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/v1/foods', foodsRouter);
 app.use('/api/v1/meals', mealsRouter);
+app.use('/api/v1/recipes/foods_search', foodsSearchRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api/v1/foods_search.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var fetch = require('node-fetch');
+
+var router = express.Router();
+
+router.get('/', (request, response) => {
+  return fetch(`https://calorie-coach-ms.herokuapp.com/graphql?query={recipes(foodType:"${request.query.q}"){id,name,recipeUrl,foodType,preparationTime,calorieCount,numberOfIngredients}}`)
+  .then(recipeData => {
+    return recipeData.json();
+  })
+  .then(parsedRecipeData => {
+    if (parsedRecipeData.data.recipes.length == 0) {
+      response.setHeader('Content-Type', 'application/json');
+      response.status(404).send(JSON.stringify({ error: `No recipes found for ${request.query.q}.` }));
+    } else {
+      response.setHeader('Content-Type', 'application/json');
+      response.status(200).send(JSON.stringify(parsedRecipeData));
+    }
+  })
+  .catch(error => {
+    response.setHeader('Content-Type', 'application/json');
+    response.status(500).send({ error });
+  })
+})
+
+module.exports = router;
