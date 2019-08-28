1643eae30108fdb57a518f6c139259456619dea5
billy urrutia <billyurrutia@gmail.com>
2019-08-28 15:34:39 -0600

    Created Route for preparation time

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var foodsRouter = require('./routes/api/v1/foods');
 var mealsRouter = require('./routes/api/v1/meals');
 var foodsSearchRouter = require('./routes/api/v1/recipes/foods_search');
 var calorieCountRouter = require('./routes/api/v1/recipes/calorie_count')
-
+var preparationTimeRouter = require('./routes/api/v1/recipes/preparation_time')
 var averageCalorieCount = require('./routes/api/v1/recipes/average_calorie_count')
 
 var app = express();
@@ -33,6 +33,7 @@ app.use('/api/v1/meals', mealsRouter);
 app.use('/api/v1/recipes/foods_search', foodsSearchRouter)
 app.use('/api/v1/recipes/calorie_count', calorieCountRouter);
 app.use('/api/v1/recipes/average_calorie_count', averageCalorieCount)
+app.use('/api/v1/recipes/preparation_time', preparationTimeRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
